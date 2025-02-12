00481d5e4cf19af44a418726dc997faf891afb32
Simon Lissack <lissack.simon@gmail.com>
2025-02-12 17:30:28 +0000

    Image to PDF

---
+++ ImageToPdf/Program.cs
@@ -0,0 +1,18 @@
+﻿using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+var document = new PdfDocument();
+
+var page = document.AddPage();
+var image = XImage.FromFile("test_image.jpg");
+
+page.Orientation = PageOrientation.Landscape;
+page.Width = XUnit.FromPoint(image.PointWidth);
+page.Height = XUnit.FromPoint(image.PointHeight);
+
+var gfx = XGraphics.FromPdfPage(page);
+
+gfx.DrawImage(image, 0, 0, image.PixelWidth, image.PixelHeight);
+
+await document.SaveAsync("out.pdf");
---
+++ readme.md
@@ -0,0 +1,1 @@
+Sample app which takes an image and writes it to a PDF as a full page. Uses [PDFsharp](https://github.com/empira/PDFsharp).
